945c257dc450a8f2f7541b97c3bcf37a211dd808
devalexsantos <devalexsantos@gmail.com>
2024-12-24 13:09:18 +0000

    feat: add move steps buttons

--- src/components/MultiStepForm.tsx
+++ src/components/MultiStepForm.tsx
@@ -20,15 +20,54 @@ export function MultiStepForm() {
 
   const steps = [
     <ServicesStep />,
+    <PersonalInformationStep />,
     <PersonalInformationStep />
   ]
 
+  const nextStep = async () => {
+    const isValid = await methods.trigger()
+    if (isValid) {
+      setCurrentStep(prev => Math.min(prev + 1, steps.length))
+    }
 
+  }
+
+  const prevStep = () => {
+    setCurrentStep(prev => Math.max(prev - 1, 1))
+  }
 
   return (
     <FormProvider {...methods}>
       <div className="w-full flex bg-zinc-50 rounded shadow p-6 justify-center" >
         {steps[currentStep - 1]}
+      </div>
+      <div className="w-full flex items-center justify-center gap-3">
+        {
+          currentStep > 1 &&
+          <button
+            onClick={prevStep}
+            className="text-sm bg-sky-800 text-white px-4 py-2 rounded-full">
+            Back
+          </button>
+        }
+        {
+          currentStep < steps.length ?
+            (
+              <button
+                onClick={nextStep}
+                className="text-sm bg-sky-800 text-white px-4 py-2 rounded-full">
+                Next
+              </button>
+            )
+            :
+            (
+              <button
+                type="submit"
+                className="text-sm bg-sky-800 text-white px-4 py-2 rounded-full">
+                Submit
+              </button>
+            )
+        }
       </div>
     </FormProvider>
   )
